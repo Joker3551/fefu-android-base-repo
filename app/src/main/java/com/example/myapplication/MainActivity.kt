7175237ac58a8963a5c14434e69a38a95af00517
Joker3551 <47964910+Joker3551@users.noreply.github.com>
2021-12-23 02:42:48 +1000

    add reg

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myapplication
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+class MainActivity : Application() {
+    companion object{
+        lateinit var INSTANCE: MainActivity
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/RegActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityRegBinding
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+
+class RegActivity : AppCompatActivity(R.layout.activity_reg){
+    private lateinit var binding: ActivityRegBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+
+}
